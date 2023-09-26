64b48359c33a0925864a22535d80a7abfd8e337e
Akhaylin Rajagopaul <akhaylin.r@gmail.com>
2023-09-26 17:46:31 -0400

    Finished step 1

---
+++ serial.py
@@ -0,0 +1,40 @@
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+
+    def __init__(self, start = 0):
+        """Initialize the starting number and current number."""
+
+        self.start = start
+        self.next  = start
+
+    def generate(self):
+        """Return the next sequential number."""
+
+        num = self.next
+        self.next += 1
+        return num
+
+    def reset(self):
+        """Reset the number back to the original start number"""
+
+        self.next = self.start
+
+
+
